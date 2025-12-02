781ad9c8c7942ba503bf9eeb49316e3111b8f7f5
anayolico <acnwa1234@gmail.com>
2025-12-02 08:34:44 +0100

    Add root route and fix Render port

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Portfolio Backend is running ✅");
+});
+
 // API route to send email
 app.post("/send-email", async (req, res) => {
   const { fullName, email, description } = req.body;
@@ -48,7 +52,7 @@ app.post("/send-email", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Backend server running on port ${PORT}`);
